93add4ec65fdbf33aa4d4252e9d8eb329dc7b081
Emeka <ilozuechukwuemeka@yahoo.com>
2022-04-07 22:26:56 +0100

    upgrade

--- src/main/java/org/lamisplus/modules/sync/controller/ClientController.java
+++ src/main/java/org/lamisplus/modules/sync/controller/ClientController.java
@@ -45,7 +45,7 @@ public class ClientController {
     private final RemoteAccessTokenService remoteAccessTokenService;
 
 
-   /* @RequestMapping(value = "/upload",
+    @RequestMapping(value = "/upload",
             method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
     @CircuitBreaker(name = "service2", fallbackMethod = "getDefaultMessage")
@@ -104,7 +104,7 @@ public class ClientController {
             }
         }
         return ResponseEntity.ok("Successful");
-    }*/
+    }
 
     public ResponseEntity<String> getDefaultMessage(Exception exception) {
         String message = exception.getMessage();
