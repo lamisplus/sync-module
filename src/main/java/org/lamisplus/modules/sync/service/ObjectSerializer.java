98b1bf6a519b53efd01f65774137babb7b03389e
Emeka <ilozuechukwuemeka@yahoo.com>
2022-04-13 16:15:27 +0100

    feat: serializer, deserializer and uuid generator

---
+++ src/main/java/org/lamisplus/modules/sync/service/ObjectSerializer.java
@@ -0,0 +1,121 @@
+package org.lamisplus.modules.sync.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.lamisplus.modules.base.domain.entity.*;
+import org.lamisplus.modules.sync.domain.mapper.*;
+import org.lamisplus.modules.base.repository.*;
+import org.lamisplus.modules.sync.domain.entity.*;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ObjectSerializer {
+    private final PatientRepository patientRepository;
+    private final VisitRepository visitRepository;
+    private final EncounterRepository encounterRepository;
+    private final FormDataRepository formDataRepository;
+    private final AppointmentRepository appointmentRepository;
+    private final PatientMapper patientMapper;
+    private final EncounterMapper encounterMapper;
+    private final FormDataMapper formDataMapper;
+    private final VisitMapper visitMapper;
+    private final AppointmentMapper appointmentMapper;
+
+    public List<?> serialize(Tables table, long facilityId, LocalDateTime dateLastSync) {
+
+        if (table.name().equals("patient")) {
+            log.info(" Retrieving records from  {} ", table.name());
+            List<Patient> patientList = new LinkedList<>();
+            if (dateLastSync == null) {
+                patientList = patientRepository.findAll();
+                System.out.println(patientList);
+            } else {
+                patientList = patientRepository.getPatientsDueForServerUpload(dateLastSync);
+            }
+            return patientList.stream()
+                    .filter(patient -> patient.getOrganisationUnitId().equals(facilityId))
+                    .map(patientMapper::toPatientDTO)
+                    .collect(Collectors.toList());
+        }
+
+
+        if (table.name().equals("visit")) {
+            log.info(" Retrieving records from  {} ", table.name());
+            List<Visit> visitList = new LinkedList<>();
+            if (dateLastSync == null) {
+                visitList = visitRepository.findAll();
+            } else {
+                visitList = visitRepository.getVisitsDueForServerUpload(dateLastSync);
+            }
+            return visitList.stream()
+                    .filter(visit -> visit.getOrganisationUnitId().equals(facilityId))
+                    .map(visit -> {
+                        Patient patient = patientRepository.findById(visit.getPatientId()).get();
+                        return visitMapper.toVisitDTO(visit, patient);
+                    }).collect(Collectors.toList());
+
+
+        }
+        if (table.name().equals("encounter")) {
+            log.info(" Retrieving records from  {} ", table.name());
+            List<Encounter> encounterList = new LinkedList<>();
+            if (dateLastSync == null) {
+                encounterList = encounterRepository.findAll();
+            } else {
+                encounterList = encounterRepository.getEncountersDueForServerUpload(dateLastSync);
+            }
+            return encounterList.stream()
+                    .filter(encounter -> encounter.getOrganisationUnitId().equals(facilityId))
+                    .map(encounter -> {
+                        Patient patient = patientRepository.findById(encounter.getPatientId()).get();
+                        Visit visit = visitRepository.findById(encounter.getVisitId()).get();
+                        return encounterMapper.toEncounterDTO(encounter, patient, visit);
+                    }).collect(Collectors.toList());
+        }
+
+        if (table.name().equals("form_data")) {
+            log.info(" Retrieving records from  {} ", table.name());
+            List<FormData> formDataList = new LinkedList<>();
+//            if (dateLastSync == null) {
+            formDataList = formDataRepository.findAll();
+//            } else {
+//                formDataList = formDataRepository.getFormDataDueForServerUpload(dateLastSync);
+//            }
+            return formDataList.stream()
+                    .filter(formData -> formData.getOrganisationUnitId().equals(facilityId))
+                    .map(formData -> {
+                        Encounter encounter = encounterRepository.findById(formData.getEncounterId()).get();
+                        return formDataMapper.toFormDataDTO(formData, encounter);
+                    }).collect(Collectors.toList());
+        }
+
+        if (table.name().equals("appointment")) {
+            log.info(" Retrieving records from  {} ", table.name());
+            List<Appointment> appointmentList = new LinkedList<>();
+            if (dateLastSync == null) {
+                appointmentList = appointmentRepository.findAll();
+            } else {
+                appointmentList = appointmentRepository.getAppointmentsDueForServerUpload(dateLastSync);
+            }
+            return appointmentList.stream()
+                    .filter(appointment -> appointment.getOrganisationUnitId().equals(facilityId))
+                    .map(appointment -> {
+                        Patient patient = patientRepository.findById(appointment.getPatientId()).get();
+                        Visit visit = visitRepository.findById(appointment.getVisitId()).get();
+                        return appointmentMapper.toAppointmentDTO(appointment, patient, visit);
+                    }).collect(Collectors.toList());
+
+        }
+        List<String> msg = new LinkedList<>();
+        msg.add("No table records was retrieved for server sync");
+        return msg;
+
+    }
+}
---
+++ src/main/java/org/lamisplus/modules/sync/service/UuidService.java
@@ -0,0 +1,84 @@
+package org.lamisplus.modules.sync.service;
+
+import lombok.RequiredArgsConstructor;
+import org.lamisplus.modules.base.domain.entity.Encounter;
+import org.lamisplus.modules.base.domain.entity.FormData;
+import org.lamisplus.modules.base.domain.entity.Patient;
+import org.lamisplus.modules.base.domain.entity.Visit;
+import org.lamisplus.modules.base.repository.EncounterRepository;
+import org.lamisplus.modules.base.repository.FormDataRepository;
+import org.lamisplus.modules.base.repository.PatientRepository;
+import org.lamisplus.modules.base.repository.VisitRepository;
+import org.lamisplus.modules.sync.domain.entity.Tables;
+import org.lamisplus.modules.sync.repo.SyncUUIDUpdateRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UuidService {
+
+    private final PatientRepository patientRepository;
+    private final VisitRepository visitRepository;
+    private final EncounterRepository encounterRepository;
+    private final FormDataRepository formDataRepository;
+    private final SyncUUIDUpdateRepository syncUUIDUpdateRepository;
+
+    @Transactional
+    public void addUuid(String table){
+        switch (table) {
+            case "patient":
+                List<Patient> patientList = patientRepository.findNullUuid();
+                List<Patient> patientList1 = new ArrayList<>();
+                patientList.forEach(patient -> {
+                    patient.setUuid(UUID.randomUUID().toString());
+                    patientList1.add(patient);
+                });
+                try {
+                    syncUUIDUpdateRepository.updateAllPatient(patientList1);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                break;
+            case "visit":
+                List<Visit> visitList = visitRepository.findNullUuid();
+                List<Visit> visitList1 = new ArrayList<>();
+                visitList.forEach(visit -> {
+                    visit.setUuid(UUID.randomUUID().toString());
+                    visitList1.add(visit);
+                });
+                syncUUIDUpdateRepository.updateAllVisit(visitList1);
+                break;
+            case "encounter":
+                List<Encounter> encounterList = encounterRepository.findNullUuid();
+                List<Encounter> encounterList1 = new ArrayList<>();
+                encounterList.forEach(encounter -> {
+                    encounter.setUuid(UUID.randomUUID().toString());
+                    encounterList1.add(encounter);
+                });
+                syncUUIDUpdateRepository.updateAllEncounter(encounterList1);
+                break;
+            case "form_data":
+                List<FormData> formDataList = formDataRepository.findNullUuid();
+                List<FormData> formDataList1 = new ArrayList<>();
+                formDataList.forEach(formData -> {
+                    formData.setUuid(UUID.randomUUID().toString());
+                    formDataList1.add(formData);
+                });
+                syncUUIDUpdateRepository.updateAllFormData(formDataList1);
+                break;
+        }
+    }
+
+    @Scheduled(fixedDelay = 50000)
+    public void getUUID(){
+        for (Tables table : Tables.values()) {
+            addUuid(table.toString());
+        }
+    }
+}
